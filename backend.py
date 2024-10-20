d93d1a93c5fd1c46969e2d3161330964aa76c68e
rakesh <rakeshsanju2000.rs@gmail.com>
2024-10-20 17:03:40 +0530

    working model

---
+++ backend.py
@@ -0,0 +1,58 @@
+import pandas as pd
+import sqlite3
+import os
+import streamlit as st
+from typing import Literal
+from dotenv import load_dotenv
+import google.generativeai as genai
+
+load_dotenv()
+genai.configure(api_key=os.getenv("api_key"))
+
+def create_styled_table(df):
+    # Convert DataFrame to HTML and add bold styling to headers
+    styled_table = df.to_html(index=False)
+    styled_table = styled_table.replace('<th>', '<th style="font-weight:bold">')
+    return styled_table
+
+def data_ingestion(dataset, table_name, type: Literal["c", "j"] = "c"):
+    if type == "c":
+        data = pd.read_csv(dataset)
+    elif type == "j":
+        data = pd.read_json(dataset)
+    fields = data.columns.to_list()
+    data.ffill(inplace=True)
+    with sqlite3.connect("mydatabase.db") as conn:
+        data.to_sql(table_name, conn, if_exists="append", index=False)
+    return fields
+
+
+def get_gemini_response(question, prompt):
+    model = genai.GenerativeModel("gemini-pro")
+    response = model.generate_content([prompt, question])
+    return response.text
+
+
+def read_sql_query(sql, db):
+    with sqlite3.connect(db) as conn:
+        cur = conn.cursor()
+        cur.execute(sql)
+        rows = cur.fetchall()
+        column_names = [description[0] for description in cur.description]
+    return column_names, rows
+
+
+def prompt_Design(table_name, fields):
+    prompt = [
+        f"""
+    you are an expert in converting english question to SQL query!
+    the SQL has a database and in that the table name is:{table_name} 
+    and has the following columns : {fields} \n\n
+    for example,\nexample 1: How many entries of records are present?,
+    the sql command will be something like this SELECT COUNT(*) FROM {table_name};
+    example 2: Tell me avg of some column?,
+    the sql command will be something like this SELECT AVG(some column) FROM {table_name};
+    the sql query should be generate from {table_name} table completely without any assumptions 
+    also the sql code should not have ```in the beginning or end and sql word is output"""
+    ]
+    return prompt
---
+++ main.py
@@ -0,0 +1,58 @@
+from backend import *
+
+st.set_page_config(page_title="SQL Query Retriever")
+st.header("Upload Your Data File")
+uploaded_file = st.file_uploader("Choose a file", type=["csv", "json"])
+if uploaded_file is not None:
+    if uploaded_file.name.endswith(".csv"):
+        file_type = "c"
+    elif uploaded_file.name.endswith(".json"):
+        file_type = "j"
+    else:
+        st.error("Unsupported file type. Please upload a CSV or JSON file.")
+    if "table_name" not in st.session_state:
+        st.session_state.table_name = ""
+    table_name = os.path.splitext(uploaded_file.name)[0]
+    st.session_state.table_name = table_name
+    if "prompt" not in st.session_state:
+        st.session_state.prompt = []
+    if "fields" not in st.session_state:
+        st.session_state.fields = []
+
+    if st.button("Ingest Data"):
+        try:
+            st.session_state.fields = data_ingestion(
+                uploaded_file, st.session_state.table_name, file_type
+            )
+            st.success(
+                f"Data ingested successfully into the '{
+                       st.session_state.table_name}' table."
+            )
+            st.session_state.prompt = prompt_Design(
+                st.session_state.table_name, st.session_state.fields
+            )
+            st.session_state.data_ingested = True
+        except Exception as e:
+            st.error(f"An error occurred: {e}")
+else:
+    st.info("please upload again")
+
+if "data_ingested" in st.session_state and st.session_state.data_ingested:
+    st.header("Gemini App to Retrieve SQL Data")
+    question = st.text_input("Enter your query")
+
+    if st.button("Get data"):
+        if question:
+            response = (
+                get_gemini_response(question, st.session_state.prompt[0])
+                .replace("```", "")
+                .replace("sql", "")
+                .strip()
+            )
+            print(st.session_state.prompt[0])
+            column, response = read_sql_query(response, "mydatabase.db")
+            st.subheader("the response is:")
+            if response:
+                df = pd.DataFrame(response, columns=column)
+                st.markdown(create_styled_table(df), unsafe_allow_html=True)
+                st.table(df)
